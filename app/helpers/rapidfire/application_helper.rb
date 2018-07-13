644cb5d3ba30218b98d34d8ebe104eeeb8f23c7c
Mada Aryakusumah <lokermada@gmail.com>
2018-07-14 05:25:21 +0700

    Update application_helper.rb

--- app/helpers/rapidfire/application_helper.rb
+++ app/helpers/rapidfire/application_helper.rb
@@ -1,8 +1,8 @@
 module Rapidfire
   module ApplicationHelper
-    def render_answer_form_helper(answer, form)
+    def render_answer_form_helper(answer, form, step = nil, last_step = nil)
       partial = answer.question.type.to_s.split("::").last.downcase
-      render partial: "rapidfire/answers/#{partial}", locals: { f: form, answer: answer}
+      render partial: "rapidfire/answers/#{partial}", locals: { f: form, answer: answer, step: ste, last_step: last_step}
     end
 
     def checkbox_checked?(answer, option)
